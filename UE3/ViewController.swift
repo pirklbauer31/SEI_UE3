a67fdb701fd061b3fc3ddd6a75bb16c0ca02e58c
Student <Student@VM-macOS-1012.local>
2017-01-26 10:13:29 +0100

    Initial Commit

---
+++ UE3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UE3
+//
+//  Created by Student on 26/01/2017.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
